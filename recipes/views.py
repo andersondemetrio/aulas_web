70e79256fa25a9c345bf384436d8bbb47a90dfb2
Anderson Demetrio Plácido <anderson.debhora@gmail.com>
2022-05-29 09:53:29 -0300

    mensagem

---
+++ recipes/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'recipes/home.html')
+
+def contato(request):
+    return HttpResponse('Contato')
+
+def sobre(request):
+    return HttpResponse('sobre')
